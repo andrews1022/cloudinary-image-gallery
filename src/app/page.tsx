35e1bfb62d4eade2cc13c3963f12be114e062bcc
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-12 20:29:58 -0700

    implemented basic upload picture functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { CldUploadButton } from "next-cloudinary";
+
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+
+      <CldUploadButton uploadPreset="qjumxp87u3" />
     </div>
   );
 };
